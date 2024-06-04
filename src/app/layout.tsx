7a506984e641585d655a20b82ddc904f624220e7
WBDev_Vivek <gawalevivek@gmail.com>
2024-06-04 17:45:29 +0530

    backsoftlightimgadd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,11 +17,10 @@ export const metadata: Metadata = {
 };
 
 
-
 // const myFont = localFont({
 //   src: './fonts/ClashDisplay_Complete/Fonts/WEB/fonts/ClashDisplay-Regular.woff2',
 // })
- 
+
 
 export default function RootLayout({
   children,
@@ -31,18 +30,26 @@ export default function RootLayout({
   return (
     <html lang="en">
 
-      <body className={`border-blue-700 pinkBorderClass  borderClass  w-full max-w-[1440px] flex mx-auto justify-end items-center `} >
-  
+      <body className={`border-blue-700 pinkBorderClass  borderClass w-full max-w-[1440px] flex mx-auto justify-end items-center relative bg-background
+      `} >
+
         <BatchSystemTaskContextProvider>
           <ThemeSwitcher>
-            <div className={`border-yellow-800 pinkBorderClass max-w-[1440px] w-full  flex flex-col items-end `} >
-            <Header />
-            {children}
-            <Footer />
+            <div className={`border-yellow-800 pinkBorderClass max-w-[1440px] w-full  flex flex-col items-end  
+            `} >
+
+              <Header />
+              {children}
+              <Footer />
             </div>
+
           </ThemeSwitcher>
         </BatchSystemTaskContextProvider>
+
+        <div className={`backBackground backSoftLightImg  border-8 border-blue-900 absolute w-full h-full mix-blend-soft-light bg-blend-soft-light`}>
+        </div>
+
       </body>
-    </html>
+    </html >
   );
 }
